bab284aff2f6edb9462c1d2c2c96c6f81d46e392
Dmitrii Limonov <disordervlz@gmail.com>
2022-08-05 02:52:32 +0300

    Add error case for token balance test

--- defi/balance_test.go
+++ defi/balance_test.go
@@ -2,6 +2,7 @@ package defi
 
 import (
 	"context"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/rovergulf/eth-contracts-go/tests"
 )
 
@@ -19,4 +20,7 @@ func (suite *DefiTestSuite) TestGetTokenAddressBalance() {
 	balance, err := GetTokenAddressBalance(ctx, suite.backend, suite.erc20Address, tests.Account0)
 	suite.Nil(err, "should not return an error on token balance check")
 	suite.NotNil(balance, "balance result shouldn't be nil")
+
+	_, err2 := GetTokenAddressBalance(ctx, suite.backend, common.HexToAddress("0x0"), tests.Account0)
+	suite.NotNil(err2, "should return error on non-existent contract or its bad address")
 }
