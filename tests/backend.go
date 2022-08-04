7e2b82f6e4aabf06370d49c5ee2b6aa03314dcef
Dmitrii Limonov <disordervlz@gmail.com>
2022-08-05 02:41:32 +0300

    Update fake backends gas limit

--- tests/backend.go
+++ tests/backend.go
@@ -8,29 +8,19 @@ import (
 
 var defaultAlloc = new(big.Int)
 
-type SimulatedBackend struct {
-	*backends.SimulatedBackend
-}
-
-func (b *SimulatedBackend) Shutdown() {
-	b.Close()
-}
-
-func NewFakeBackend() *SimulatedBackend {
+func NewFakeBackend() *backends.SimulatedBackend {
 	defaultAlloc.SetString("1000000000000000000000", 10)
-	return &SimulatedBackend{
-		SimulatedBackend: backends.NewSimulatedBackend(core.GenesisAlloc{
-			Account0:  core.GenesisAccount{Balance: defaultAlloc},
-			Account1:  core.GenesisAccount{Balance: defaultAlloc},
-			Account2:  core.GenesisAccount{Balance: defaultAlloc},
-			Account3:  core.GenesisAccount{Balance: defaultAlloc},
-			Account4:  core.GenesisAccount{Balance: defaultAlloc},
-			Account5:  core.GenesisAccount{Balance: defaultAlloc},
-			Account6:  core.GenesisAccount{Balance: defaultAlloc},
-			Account7:  core.GenesisAccount{Balance: defaultAlloc},
-			Account8:  core.GenesisAccount{Balance: defaultAlloc},
-			Account9:  core.GenesisAccount{Balance: defaultAlloc},
-			Account10: core.GenesisAccount{Balance: defaultAlloc},
-		}, 32000),
-	}
+	return backends.NewSimulatedBackend(core.GenesisAlloc{
+		Account0:  core.GenesisAccount{Balance: defaultAlloc},
+		Account1:  core.GenesisAccount{Balance: defaultAlloc},
+		Account2:  core.GenesisAccount{Balance: defaultAlloc},
+		Account3:  core.GenesisAccount{Balance: defaultAlloc},
+		Account4:  core.GenesisAccount{Balance: defaultAlloc},
+		Account5:  core.GenesisAccount{Balance: defaultAlloc},
+		Account6:  core.GenesisAccount{Balance: defaultAlloc},
+		Account7:  core.GenesisAccount{Balance: defaultAlloc},
+		Account8:  core.GenesisAccount{Balance: defaultAlloc},
+		Account9:  core.GenesisAccount{Balance: defaultAlloc},
+		Account10: core.GenesisAccount{Balance: defaultAlloc},
+	}, 4712388)
 }
